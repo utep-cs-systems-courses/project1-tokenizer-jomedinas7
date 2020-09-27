018f095206c2e16a1f1fe5ce8db1cd026f91f50d
jomedinas7 <jmedinas@miners.utep.edu>
2020-09-26 23:05:38 -0600

    added comments to tokenizer.c and history.c files

--- src/history.c
+++ src/history.c
@@ -4,6 +4,7 @@
 
 List *init_history()
 {
+  /* allocate memory for the List and set the root to 0 to indicate there are no Items in the list yet*/
   List *hist = malloc(sizeof(List));
   hist -> root = 0;
   return hist;
@@ -11,64 +12,70 @@ List *init_history()
 
 void add_history(List *list, char *str)
 {
-  int nodeID = 1;
-  Item *ptr = list->root;
-  if(ptr == 0)
-  {
-    //It is what it is...
+  /* given the string we point a pointer at the start of the word, and increment a counter as long as there are characters in the string */
+  int strLen = 0;
+  char *strPtr = str;
+  while(*strPtr++){
+      strLen++;
   }
+  /* now knowing the size of the string, we know how many character sized slots we need to allocate memory for a copy of the string +1 for the \0 character*/
+  char *strCopy = (char*)malloc((strLen+1)*sizeof(char));
+  for(int i = 0; i<strLen;i++){
+    strCopy[i] = str[i];
+  }
+  strCopy[strLen+1] = '\0';
+
+  /*since the first Item node will be added to the history, we can say that the nodeId will be 1*/
+  int nodeID = 1;
+  /* allocate a slot of memory of size Item for the new node Item*/
+  Item *newNode = malloc(sizeof(Item));
+  /*assign the new Node Item's str and next attributes with the copy of the string, and 0 respectively to indicate the end of the list*/
+  newNode ->str = strCopy;
+  newNode ->next = 0;
+
+  /*point a pointer to the root of the list, if the pointer points to a value 0, then we can place our newNode Item as the root */
+  Item *ptr = list->root;
+  if(list->root == 0)
+  {
+    newNode -> id = nodeID;
+    list -> root = newNode;
+  }
+  /*increment the nodeID as long as there are list Items to point to, if the next item in the list is 0, we will know to place our new Node Item there*/
   else{
-    while(ptr ->next != 0)
+    nodeID++;
+    while(ptr->next != 0)
     {
 	ptr = ptr->next;
 	nodeID++;
     }
+    newNode -> id =nodeID;
+    ptr ->next = newNode;
   }
-  Item *newNode = malloc(sizeof(Item));
-  newNode -> id = nodeID;
-  char* strEnd = str;
-  while(*strEnd++)
-  {
-    ;
-  }
-  char* strCopy = (char *)malloc((strEnd-str)*sizeof(char));
-  int i;
-  for(i =0; i < strEnd-str;i++)
-  {
-    strCopy[i] = str[i];
-  }
-  strCopy[i] = '\0';
-  newNode ->str = strCopy;
   
-  newNode ->next = 0;
-  if(ptr == 0){
-    list->root = newNode;
-  }
-  else{
-  ptr ->next = newNode;
-  }
 }
 
 char *get_history(List *list, int id)
 {
+  /*given that the root ID will be 1, we can increment a counter alongside traversal of the list until the Item ID matches the id passed in the parameters*/
   Item *ptr = list -> root;
   int nodeIdx = 1;
   while(ptr != 0)
   {
-    if(nodeIdx== id)
+    if(ptr->id == id)
       {
 	return ptr->str;
       }
     ptr = ptr -> next;
     nodeIdx++;
   }
+  /*if the id we were given is not found because it is either too big or non-existent, we return this statement to be printed*/
   return "That history ID was not found";
 }
 
 void print_history(List *list)
 {
+  /*print the ID of the list Item alongside with the string that the Item holds, traversing the list until we reach 0*/
   Item *ptr = list -> root;
-  // printf("This is the root: %s\n",ptr->str);
   while(ptr!=0)
   {
     printf("ID: %d, %s\n",ptr -> id,ptr -> str);
@@ -77,6 +84,7 @@ void print_history(List *list)
 }
 void free_history(List *list)
 {
+  /*we have to free every slot of allocated memory for each Item in the List, and then free the list itself*/
   Item *ptr = list -> root;
   Item *temp;
   while(ptr != 0){
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -22,23 +22,22 @@ int non_space_char(char c)
 char *word_start(char *str)
 {
   int index = 0;
-  //if there is a space at the beginning of the string, this will skip over them until a character is found
-  while(space_char(str[index]))
-  {
+  /*if there is a space at the beginning of the string, this will skip over them until a character is found*/
+  while(space_char(str[index])){
     index++;
   }
-    // str = &str[index];
-    //returning the same pointer str, but modified to point to the first character of the word  
+  /*returning the same pointer str, but modified to point to the first character of the word*/  
   return str+index;
 }
 
 char *word_terminator(char *str)
 {
   int index = 0;
-  while(non_space_char(str[index]))
-  {
+  /*we use this loop to know the length of the word until we hit a space character*/
+  while(non_space_char(str[index])){
     index++;
   }
+  /*returning the str pointer plus the indices to point it to the space character after the word*/
     return str+index;
 }
 
@@ -46,19 +45,29 @@ int count_words(char *str)
 {
   int num_words = 0;
   char *ptr = str;
-  while(space_char(*ptr))
-  {
+  
+  /*Initially need to skip any premptive spaces that the user may have inputted*/
+  while(space_char(*ptr)){
+    /*if we're still pointing to spaces, but the next character indicates the end of the string then we should return 0 since no word was found*/
+    if(ptr[1] == '\0'){
+      return 0;
+    }
     ptr++;
   }
-  if(*ptr == '\0')
-  {
+  /*if we are not pointing to space characters, but end up with an empty string, then return 0*/
+  if(ptr[0] == '\0'){
     return num_words; 
   }
-  while(*ptr != '\0')
-  {
+  while(*ptr != '\0') {
+    /*point to the beginning of the next word*/
     ptr = word_start(ptr);
+    /*point to the end of the word*/
     ptr = word_terminator(ptr);
+    /*having been through a word from start to finish we can say that we can increment the number of words*/
     num_words++;
+    if(ptr[0] == '\0'){
+      return num_words;
+    }
   }
   return num_words;
 }
@@ -66,16 +75,18 @@ int count_words(char *str)
 char* copy_str(char *inStr, short len)
 {
   char *p;
+  /*allocate memory for a copy of the string we recieve using as many char sized slots as there are in the word +1 for the \0 character*/
   p = (char *)malloc((len+1)*sizeof(char));
   if(!p){
     printf("Memory allocation failed");
   }
+  /*using a loop to assign individual character values of the string to our newly allocated memory*/
   for(int i = 0; i<len; i++)
   {
     p[i] = inStr[i];
   }
+  /*signaling the ending of our string copy with the \0 character*/
   p[len+1]= '\0';
-  // printf("Copied String:%s\n",p);
   return p;
 
 }
@@ -86,13 +97,18 @@ char **tokenize(char *inStr)
   char **tokens = (char**)malloc(sizeof(char*)*(numWords+1));
   char* wordProg = inStr;
   int i = 0;
-  for(i = 0; i <numWords; i++)
-  {
+  while(space_char(*wordProg)){
+     wordProg++;
+    }
+  /*this loop will copy words one at a time using the copy_str method, we use the difference in indices between the pointers from word_terminator and word_start as the length of the word*/
+  for(i = 0; i <numWords; i++){
     tokens[i] = copy_str(wordProg,(word_terminator(wordProg)-word_start(wordProg)));
+    /*Here we move the pointer through the words similar to how we did in count_words, but now we make sure to start the loop with the pointer at the beginning of the next word */
     wordProg = word_start(wordProg);
     wordProg = word_terminator(wordProg);
     wordProg = word_start(wordProg);
   }
+  /*Making sure to indicate the end of the token array with the \0 character*/
   tokens[i] = '\0';
   return tokens;
   
@@ -109,20 +125,14 @@ void print_tokens(char **tokens)
 }
 void free_tokens(char **tokens)
 {
+  /*Every malloc needs to have a free, so we free the memory used for every word inside of the tokens array, then we free the memory allocated for the tokens array itself*/
   for(int i = 0; tokens[i];i++)
   {
     free(tokens[i]);
     tokens[i] = NULL;
-    if(!tokens[i]){
-      printf("memory freed!\n");
-    }
   }
   free(tokens);
   tokens = NULL;
-  if(!tokens)
-  {
-    printf("Malloc Memory successfully freed.\n");
-  }
 }
 
 
